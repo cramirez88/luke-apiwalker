1a30863df00c0b0bf30e559ea562fee752681073
Christian Ramirez <christian.r0602@gmail.com>
2023-01-09 19:53:09 -0600

    error api message

--- src/components/People.js
+++ src/components/People.js
@@ -4,6 +4,7 @@ import {useParams} from 'react-router-dom'
 
 export default function People() {
   const {id} = useParams()
+  const [error, setError] = useState('')
   const [apiData, setApiData] = useState({})
   useEffect(() => {
     axios.get(`https://swapi.dev/api/people/${id}`)
@@ -11,14 +12,34 @@ export default function People() {
       console.log(res.data)
       setApiData(res.data)
     })
+    .catch(err => {
+      console.log('this is the people\'s component error' + err)
+      setError('This is the people\'s component error')
+    })
   },[])
+  if(!error){
+    return (
+      <>
+      <div>
+       <p>{apiData.name}</p>
+       <p>{apiData.eye_color}</p>
+       <p>{apiData.skin_color}</p>
+       <p>{apiData.birth_year}</p>
+      
+      </div>
 
-  return (
-    <div>
-     <p>{apiData.name}</p>
-     <p>{apiData.eye_color}</p>
-     <p>{apiData.skin_color}</p>
-     <p>{apiData.birth_year}</p>
-    </div>
-  )
+      </>
+    )
+  }else {
+    return (
+      <>
+        <img src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fgetwallpapers.com%2Fwallpaper%2Ffull%2F8%2F2%2Fb%2F312588.jpg&f=1&nofb=1&ipt=4e08c4472b99abcc3979f1e9361222003cb9c73d5121b6bb79e9ab848ab87c8c&ipo=images' style={{width: '200px'}}></img>
+      <h3>These aren't the droids you are looking for!</h3>
+      </>
+      
+    ) 
+  }
+
+
+ 
 }
