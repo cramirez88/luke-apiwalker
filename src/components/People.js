2af216cd666788fe2c928c0927621f5c040f6db7
Christian Ramirez <christian.r0602@gmail.com>
2023-01-10 20:20:23 -0600

    extracted id to incorprate into link

--- src/components/People.js
+++ src/components/People.js
@@ -1,28 +1,43 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import {useParams} from 'react-router-dom'
+import {useParams, Link} from 'react-router-dom'
 
 export default function People() {
   const {id} = useParams()
   const [error, setError] = useState('')
   const [apiData, setApiData] = useState({})
-  const [homeworld, setHomeworld] = useState([])
+  const [homeworld, setHomeworld] = useState('')
+  const [urlId, setUrlId] = useState()
   useEffect(() => {
     axios.get(`https://swapi.dev/api/people/${id}`)
     .then(res => {
       console.log(res.data)
       setApiData(res.data)
-      
+      getIdFromURL(res.data.homeworld)
+      axios.get(res.data.homeworld)
+      .then(res => {
+        
+        setHomeworld(res.data.name)
+      })
     })
-    axios.get(`https://swapi.dev/api/planets/${id}`)
-    .then(res => {
-      console.log(res)
-      setHomeworld(res.data.name)
-    })
+   
     .catch(err => {
       console.log('this is the people\'s component error' + err)
       setError('This is the people\'s component error')
     })
+
+
+    const getIdFromURL = (url) => {
+      if(url.charAt(url.length -3) === '/'){
+        const firstDigit = url.charAt(url.length - 2)
+        setUrlId(firstDigit)
+        console.log(firstDigit)
+      }else {
+        const firstDigit = url.charAt(url.length - 3)
+        const secondDigit = url.charAt(url.length -2 )
+        setUrlId(`${firstDigit}${secondDigit}`)
+      }
+    }
     
   },[id])
   if(!error){
@@ -34,6 +49,7 @@ export default function People() {
        <p>{apiData.skin_color}</p>
        <p>{apiData.birth_year}</p>
       <p>{homeworld}</p>
+      <Link to={`/planets/${urlId}`}>Homeworld</Link>
       </div>
 
       </>
