98b09166c80cbf17d8027ddd8c06e1806aa852fa
Christian Ramirez <christian.r0602@gmail.com>
2023-01-17 21:13:19 -0600

    SCRATCH

--- src/App.js
+++ src/App.js
@@ -1,27 +1,8 @@
 import React from 'react'
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  useParams,
-  useNavigate
-} from 'react-router-dom'
-import People from './components/People';
-import Planets from './components/Planets';
-import Form from './components/Form';
 
 function App() {
   return (
    <>
-   <BrowserRouter>
-   <Form/>
-   
-    <Routes>
-      <Route path='/people/:id' element={<People/>}/>
-      <Route path='/planets/:id' element={<Planets/>}/>
-    </Routes>
-   </BrowserRouter>
- 
 
    </>
   );
